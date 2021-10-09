a0fbcdf55e35593b8b17511a7c157b9d0797272b
Daniel Borges <lemebor91@yahoo.com>
2021-10-09 18:13:57 -0300

    Usando Sql

---
+++ curso-golang/sql/update_delete/update_delete.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:123456@/cursogo")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	//Update
+	stmt, _ := db.Prepare("update usuarios set nome = ? where id= ?")
+	stmt.Exec("novo usuario 1", 1)
+	stmt.Exec("novo usuario 2", 2)
+
+	//delete
+	stmt.Exec("delete from usuarios where id = ?")
+	stmt.Exec(3)
+}
